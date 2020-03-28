cffab2bc0a8aaf3b184c6514f5b38a03f3704de9
dothuthanh <55688858+dothuthanh@users.noreply.github.com>
2020-03-28 17:35:54 +0700

    Add files via upload

---
+++ MovablePoint.java
@@ -0,0 +1,45 @@
+package packe;
+
+public class MovablePoint extends Point {
+    private float xSpeed = 0.0f;
+    private float ySpeed = 0.0f;
+    public MovablePoint(){
+
+    }
+
+    public float getxSpeed() {
+        return xSpeed;
+    }
+
+    public void setxSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getySpeed() {
+        return ySpeed;
+    }
+
+    public void setySpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+    public void move(){
+       x += xSpeed;
+        y += ySpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "MovablePoint{" +
+                "xSpeed=" + xSpeed +
+                ", ySpeed=" + ySpeed +
+                ", x=" + x +
+                ", y=" + y +
+                '}'+ super.toString();
+    }
+}
---
+++ Test.java
@@ -0,0 +1,12 @@
+package packe;
+
+public class Test {
+    public static void main(String[] args) {
+        Point point = new Point();
+        point = new Point(4.5f,5.0f);
+        System.out.println(point);
+        MovablePoint move = new MovablePoint();
+        move = new MovablePoint(3.5f,3.4f,6.2f,4.5f);
+        System.out.println(move);
+    }
+}
